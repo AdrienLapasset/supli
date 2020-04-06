3732f6a733b8a16fbfdaee9c6a5f7da766413891
Adrien Lapasset <aflapasset@gmail.com>
2020-04-06 12:28:00 +0200

    add tiny slider

--- js.js
+++ js.js
@@ -4,5 +4,13 @@ var slider = tns({
   center: true,
   loop: false,
   swipeAngle: false,
-  speed: 400
+  speed: 400,
+  mouseDrag: true,
+  navPosition: "bottom",
+  controls: false,
+  arrowKeys: true,
+  gutter: 40,
+  navAsThumbnails: true,
+  // navContainer: ".features__slider__navContainer"
+  // fixedWidth: 190,
 });
