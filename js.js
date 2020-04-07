5fbff36e8e3a4a0bf2ffef52d5475d7e5a955a2f
Adrien Lapasset <aflapasset@gmail.com>
2020-04-07 11:38:33 +0200

    add animations

--- js.js
+++ js.js
@@ -1,16 +1,36 @@
-var slider = tns({
-  container: '.features__slider',
-  items: 2,
-  center: true,
-  loop: false,
-  swipeAngle: false,
-  speed: 400,
-  mouseDrag: true,
-  navPosition: "bottom",
-  controls: false,
-  arrowKeys: true,
-  gutter: 40,
-  navAsThumbnails: true,
-  navContainer: ".features__slider__navContainer",
-  // fixedWidth: 190,
-});
+$(document).ready(function () {
+
+  var slider = tns({
+    container: '.features__slider',
+    items: 2,
+    center: true,
+    loop: false,
+    swipeAngle: false,
+    speed: 400,
+    mouseDrag: true,
+    navPosition: "bottom",
+    controls: false,
+    arrowKeys: true,
+    gutter: 40,
+    navAsThumbnails: true,
+    navContainer: ".features__slider__navContainer",
+    // fixedWidth: 190,
+  });
+
+
+  var logos = $('.trust__logos');
+
+  $(window).scroll(function () {
+    var top_of_element = logos.offset().top;
+    var bottom_of_element = logos.offset().top + logos.outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+      logos.addClass("trust__logos--show");
+    } else {
+    }
+  });
+
+
+})
