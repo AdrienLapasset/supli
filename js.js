f1eecc88d2e9f767f906dee02fe4999d6372f190
Adrien Lapasset <aflapasset@gmail.com>
2020-04-09 12:24:14 +0200

    responsive

--- js.js
+++ js.js
@@ -43,4 +43,17 @@ $(document).ready(function () {
     } else {
     }
   }
+
+  $("a[href*='#']:not([href='#'])").click(function () {
+    if (
+      location.hostname == this.hostname
+      && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
+    ) {
+      var anchor = $(this.hash);
+      anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
+      if (anchor.length) {
+        $("html, body").animate({ scrollTop: anchor.offset().top - 100 }, 1500);
+      }
+    }
+  });
 })
