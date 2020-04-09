5358916ff83ae31f7357b3e11786c306309a6419
Adrien Lapasset <aflapasset@gmail.com>
2020-04-09 16:07:58 +0200

    add js comments

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+  // Tiny Slider https://github.com/ganlanyuan/tiny-slider
   var slider = tns({
     container: '.features__slider',
     items: 2,
@@ -14,12 +15,9 @@ $(document).ready(function () {
     gutter: 40,
     navAsThumbnails: true,
     navContainer: ".features__slider__navContainer",
-    // fixedWidth: 190,
   });
 
-
-  // var logos = $('.trust__logos');
-
+  // Add class "show" on elements
   $(window).scroll(function () {
     isVisible($('.trust__logos'))
     isVisible($('.comment'))
@@ -31,19 +29,18 @@ $(document).ready(function () {
     isVisible($('.partner'))
   });
 
-
   var isVisible = function (el) {
     var top_of_element = el.offset().top;
     var bottom_of_element = el.offset().top + el.outerHeight();
     var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
     var top_of_screen = $(window).scrollTop();
-
     if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
       el.addClass("show");
     } else {
     }
   }
 
+  // Anchor smooth scroll
   $("a[href*='#']:not([href='#'])").click(function () {
     if (
       location.hostname == this.hostname
